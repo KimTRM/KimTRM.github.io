8efbbd418f37c66ee49a2d00caf0ff1a4c86fc52
Kim Louise <kimlabrador71@gmail.com>
2025-06-19 16:45:11 +0800

    Add ServicesSection to About page and create ContactSection styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
 import AboutSection from "../home/sections/AboutSection";
+import ServicesSection from "../home/sections/ServicesSection";
 
 export default function About() {
     return (
-        <AboutSection />
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
     );
 }
--- app/home/sections/ContactSection.tsx
+++ app/home/sections/ContactSection.tsx
@@ -1,10 +1,10 @@
-import "../css/style.css"
+import "../css/ContactSection.css"
 
 export default function ContactSection() {
     return (
         <>
             <section id="contact" className="contact">
-                <div className="container">
+                <div className="containerc">
                     <h2>Let&apos;s Work Together</h2>
                     <p>Have a project in mind or just want to say hi? Let’s talk!</p>
                     <a href="mailto:kimlabrador71@gmail.com" className="btn">Email Me</a>
